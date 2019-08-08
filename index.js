9949041691f688fa6e99d731cfb24e04f2ee0c1f
Oleksandr Yanov <oleksandr_yanov1@epam.com>
2019-08-08 16:54:49 +0300

    Added using S3 bucket

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const app = express();
-const fileUpload = require('express-fileupload');
+const express = require('express'),
+	app = express(),
+	fileUpload = require('express-fileupload'),
+	AWS = require("aws-sdk"),
+	s3 = new AWS.S3();
 
 app.use(fileUpload());
 app.set('view engine', 'ejs');
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(80, () => console.log('listening on port 80'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('index');
 });
 
-app.post('/upload', function(req, res) {
-    var file = req.files.file;
-    res.send(file.name);
+app.post('/upload', function (req, res) {
+	var file = req.files.file;
+
+	const params = {
+		Bucket: "ec2poc",
+		Key: file.name,
+		Body: file.data
+	};
+
+	s3.upload(params, function(s3Err, data) {
+		if (s3Err) throw s3Err;
+		console.log(`File uploaded successfully at ${data.Location}`)
+	});
+
+	res.send(file.name);
 });
