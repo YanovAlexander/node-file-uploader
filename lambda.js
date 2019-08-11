6bf8a23e2d5b1d91c6aae03264c8e9f5887c31b8
yanov.alexander <yanov.alexander@gmail.com>
2019-08-11 12:35:40 +0300

    Updated lambda to use SNS with ARN.

--- lambda.js
+++ lambda.js
@@ -1,61 +1,47 @@
 var AWS = require("aws-sdk"),
-	s3 = new AWS.S3(),
-	ses = new AWS.SES();
+    s3 = new AWS.S3(),
+    ses = new AWS.SES();
 
 exports.handler = async (event, context, callback) => {
-	// TODO implement
-	var response = {
-		statusCode: 200,
-		body: JSON.stringify('Hello from Lambda!')
-	};
-
-	var bucketParams = {
-		Bucket : "ec2poc"
-	};
-
-	var sum = 0;
-
-	s3.listObjectsV2(bucketParams, function(err, data){
-		if (err) {
-			console.log("Error", err);
-		} else {
-			data.Contents.forEach(function(object){
-				sum += object.Size;
-				console.log("OBJECT " + object.Size);
-			});
-			console.log("SUM " + sum);
-			var params = {
-				Destination: {
-					ToAddresses: ["yanov.alexander@gmail.com"]
-				},
-				Message: {
-					Body: {
-						Text: { Data: "Test"
-
-						}
-
-					},
-
-					Subject: { Data: "Test Email"
-
-					}
-				},
-				Source: "`=?utf-8?B?${fromBase64}?= <fromemail@someemail.com>`"
-			};
-			ses.sendEmail(params, function (err, data) {
-				callback(null, {err: err, data: data});
-				if (err) {
-					console.log(err);
-					context.fail(err);
-				} else {
-
-					console.log(data);
-					context.succeed(event);
-				}
-			});
-		}
-	});
-
-
-	return response;
+    // TODO implement
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!')
+    };
+    
+    var bucketParams = {
+        Bucket : "ec2poc"
+    };
+    
+    var sum = 0;
+    
+    s3.listObjectsV2(bucketParams, function(err, data){
+        if (err) {
+    console.log("Error", err);
+        } else {
+        data.Contents.forEach(function(object){
+            sum += object.Size;
+            console.log("OBJECT " + object.Size);
+        });
+        console.log("SUM " + sum);
+        
+        var params = {
+            Message: 'TEXT_MESSAGE', /* required */
+            TopicArn: 'arn:aws:sns:us-east-1:273610051498:NotifyMe',
+            };
+        var publishTextPromise = new AWS.SNS().publish(params).promise();
+        
+        publishTextPromise.then(
+          function(data) {
+            console.log("SUCCESS SEND TO ARN: MessageID is " + data.MessageId);
+          }).catch(
+            function(err) {
+            console.error(err, err.stack);
+          });
+        }
+    });
+    
+    
+    return response;
 };
+
